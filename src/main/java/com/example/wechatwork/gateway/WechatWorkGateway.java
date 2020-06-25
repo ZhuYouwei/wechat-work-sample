3d6fbabafb49a78fb1eaecea22e91b14581e104a
Hermann Yung <hermannyung@gmail.com>
2020-06-26 00:01:35 +0800

    Sends welcome message for new clients; add disclaimer template for the employee to send to the new client

--- src/main/java/com/example/wechatwork/gateway/WechatWorkGateway.java
+++ src/main/java/com/example/wechatwork/gateway/WechatWorkGateway.java
@@ -2,6 +2,9 @@ package com.example.wechatwork.gateway;
 
 import com.example.wechatwork.config.WechatWorkConfig;
 import com.example.wechatwork.model.GetTokenResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import me.chanjar.weixin.common.api.WxConsts;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +12,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.util.Arrays;
 import java.util.HashMap;
 
 @Component
+@Slf4j
 public class WechatWorkGateway {
     @Autowired
     private WechatWorkConfig config;
@@ -52,6 +61,65 @@ public class WechatWorkGateway {
                         .queryParam("access_token", accessToken)
                         .build())
                 .retrieve();
+
+        return response.bodyToMono(String.class).block();
+    }
+
+
+    public String sendMessageTask(String accessToken, String externalUserId, String text) {
+        WebClient client = WebClient
+                .builder()
+                .baseUrl("https://qyapi.weixin.qq.com/")
+                .defaultCookie("cookieKey", "cookieValue")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        val body = new HashMap<String, Object>();
+        body.put("chat_type", "single");
+
+        val content = new HashMap<String, String>();
+        content.put("content", text);
+        body.put("text", content);
+
+        val externalUserIds = Arrays.asList(externalUserId);
+        body.put("external_userid", externalUserIds);
+
+        WebClient.ResponseSpec response;
+        response = client.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/cgi-bin/externalcontact/add_msg_template")
+                        .queryParam("access_token", accessToken)
+                        .build())
+                .body(Mono.just(body), HashMap.class)
+                .retrieve();
+
+        return response.bodyToMono(String.class).block();
+    }
+
+    public String sendWelcome(String accessToken, String welcomeCode, String text)  {
+        WebClient client = WebClient
+                .builder()
+                .baseUrl("https://qyapi.weixin.qq.com/")
+                .defaultCookie("cookieKey", "cookieValue")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+
+        val body = new HashMap<String, Object>();
+        body.put("welcome_code", welcomeCode);
+
+        val content = new HashMap<String, String>();
+        content.put("content", text);
+        body.put("text", content);
+
+        WebClient.ResponseSpec response;
+        response = client.post()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/cgi-bin/externalcontact/send_welcome_msg")
+                        .queryParam("access_token", accessToken)
+                        .build())
+                .body(Mono.just(body), HashMap.class)
+                .retrieve();
+
 
         return response.bodyToMono(String.class).block();
     }
